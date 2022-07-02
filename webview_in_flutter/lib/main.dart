0354b04570c8c1e6df765b84c8e364320de82606
Andy Ng <andy.ng@betterpi.com>
2022-07-02 19:06:25 +0800

    Init flutter project and install webview package. Also putting a WebView on the screen

---
+++ webview_in_flutter/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: WebViewApp(),
+  ));
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({Key? key}) : super(key: key);
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
+      ),
+      // Putting a Webview on the screen
+      body: const WebView(
+        initialUrl: 'htttps://flutter.dev',
+      ),
+    );
+  }
+}
